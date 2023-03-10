2764f9c9c2fcce2f18ccdb03bd2c9b8467d49360
AlainYRS <galain0106@gmail.com>
2023-03-09 23:11:55 -0500

    Progressive Web Application enabled, Manifest, Service Worker and Icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+import withPWA from 'next-pwa';
+import runtimeCaching from 'next-pwa/cache.js';
 
-module.exports = nextConfig
+const config = {
+  reactStrictMode: false,
+  swcMinify: true,
+};
+
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: false, //process.env.NODE_ENV === 'development', // production, true(disable SW in Production and Development), false(generate SW in Production and Development)
+  register: true,
+  scope: '/',
+  skipWaiting: true,
+  runtimeCaching,
+  sw: 'service-worker.js',
+})(
+  config
+);
+
+export default nextConfig;
