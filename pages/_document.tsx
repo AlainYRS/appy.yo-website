0ee3277a8e62af53d1240e153f3407dfbb820786
AlainYRS <galain0106@gmail.com>
2023-03-19 21:17:14 -0400

    Adjutsment for responsive design

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,15 +5,12 @@ export default function Document() {
     <Html lang="en">
       <Head >
         <link rel="manifest" href="/manifest.json" /> {/*Linking manifest file to make the web application as an installable PWA*/}
-        <meta name="theme-color" content="#2F303A" />
         <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/Icons/manifest_icons/MaskableLogo.png" />
         <link rel="apple-touch-icon" type="image/png" sizes="48x48" href="/Icons/manifest_icons/Logo48.png" />
         <link rel="apple-touch-icon" type="image/png" sizes="72x72" href="/Icons/manifest_icons/Logo72.png" />
         <link rel="apple-touch-icon" type="image/png" sizes="96x96" href="/Icons/manifest_icons/Logo96.png" />
         <link rel="apple-touch-icon" type="image/png" sizes="144x144" href="/Icons/manifest_icons/Logo144.png" />
         <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/Icons/manifest_icons/Logo192.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv='ScreenOrientation' content='autoRotate:disabled' />
       </Head>
       <body>
         <Main />
